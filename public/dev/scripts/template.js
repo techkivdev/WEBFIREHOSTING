50a7a976e2e41d716ee8c187ec5e6dfe2f99d9e6
techkivdev <techkivdev@gmail.com>
2019-10-07 11:04:39 +0530

    changes

--- public/dev/scripts/template.js
+++ public/dev/scripts/template.js
@@ -1,5 +1,5 @@
 // *******************************************************************************
-// SCRIPT : index_dev.js
+// SCRIPT : index.js
 //
 //
 // Author : Vivek Thakur
@@ -10,26 +10,38 @@
 // ------------- CONFIGURATION ----------------
 var basePath = '/DATABASE/DEVELOPMENT/PUBLIC/';
 var baseProductionPath = '/DATABASE/PRODUCTION/PUBLIC/';
-var baseTestingPath = '/DATABASE/TESTING/PUBLIC/';
 
 var imagebasePath = '/DATABASE/DEVELOPMENT/PUBLIC/';
 var imagebaseProductionPath = '/DATABASE/PRODUCTION/PUBLIC/';
-var imagebaseTestingPath = '/DATABASE/TESTING/PUBLIC/';
 
 // ********************************************
-// ------------ INPUT DATA --------------------
-console.log('-------- index.js ---------')
+// ------------ Mode Configuration -----------
+// Debug Mode
 var debug_mode = true
 
+// Change Mode for Production or Development
+var is_production_mode = false
+
 // ---------- Main Variables ---------
-var coll_base_path = basePath // Use production path for prod js file
+var coll_base_path = basePath
 
-var coll_lang = 'NA';
-var coll_name = 'NA';
-var user_role = 'NA';
-var document_ID = 'NA';
+if(is_production_mode) {
+  coll_base_path = baseProductionPath
+}
 
+// -------- Link Page with Collection and Documents -----
+var coll_lang = 'CORE';
+var coll_name = 'COLLECTION2_DATA';
+var user_role = 'DEV';
+var document_ID = 'DOC0';
+
+// Global Data variables
+// All documents data
 var allDocCmpData = {}
+
+// Mapping Data
+var mainDocMapDetails = {}
+var docMapDetails = {}
 
 // ********************************************
 
@@ -55,16 +67,8 @@ function getParams(){
     coll_name = params['coll_name'];
     user_role = params['role'];
     document_ID = params['docID'];
-  } else {
-    // Use default value for testing purpose
-    if(debug_mode){
-      coll_lang = 'CORE';
-      coll_name = 'COLLECTION2_DATA'
-      user_role = 'DEV'
-      document_ID = 'DOC0'
-    }
-  }
-  
+  } 
+
   displayOutput(coll_lang +' , '+ coll_name + ' , ' + user_role + ' , ' + document_ID)
 	
 	
@@ -157,6 +161,8 @@ async function readDocumentDataAsync(docID) {
       } else {
         displayOutput(docID + ' - Document data Read Done.');
         allDocCmpData[docID] = doc.data()
+
+        mapDocumentDetails(docID)
       }
     })
     .catch(err => {
@@ -177,6 +183,8 @@ async function readDocumentDataAsync(docID) {
       } else {
         displayOutput('MAIN - Document data Read Done.');
         allDocCmpData['MAIN'] = doc.data()
+
+        mapMAINDocDetails()
 
         hidePleaseWait()
 
@@ -213,12 +221,160 @@ readDocumentDataAsync(document_ID)
 
 // *******************************************************
 // --------------- Functions -----------------------------
+
+// Update Complete HTML Page
 function updateHTMLPage() {
 
   displayOutput(allDocCmpData)
+  displayOutput(mainDocMapDetails)
+  displayOutput(docMapDetails)
+
+  // HTML Modification functions
+  updateHeaderImages()
+  
 
 }
 
+// *******************************************************
+// --------------- Mapping Functions ---------------------
+
+// MAP MAIN Document Details
+function mapMAINDocDetails() {  
+
+  if('MAIN' in allDocCmpData) {
+
+    mainDocMapDetails['ID'] = allDocCmpData['MAIN']['INFO0']
+    mainDocMapDetails['NAME'] = allDocCmpData['MAIN']['INFO1']
+    mainDocMapDetails['DESC'] = allDocCmpData['MAIN']['INFO2']
+    mainDocMapDetails['VISIBLE'] = allDocCmpData['MAIN']['INFO3']
+    mainDocMapDetails['OWNER'] = allDocCmpData['MAIN']['INFO4']
+    mainDocMapDetails['LINKS'] = allDocCmpData['MAIN']['INFO5']
+    mainDocMapDetails['TDOC'] = allDocCmpData['MAIN']['INFO6']
+    mainDocMapDetails['ADMIN'] = allDocCmpData['MAIN']['INFO7']
+    mainDocMapDetails['IMAGE_TAB'] = allDocCmpData['MAIN']['INFO8']
+    mainDocMapDetails['MULTI_TAB'] = allDocCmpData['MAIN']['INFO9']
+    mainDocMapDetails['FORM_TAB'] = allDocCmpData['MAIN']['INFO10']
+    mainDocMapDetails['COLL_DOC'] = allDocCmpData['MAIN']['INFO11']
+    mainDocMapDetails['DOC_PBLS'] = allDocCmpData['MAIN']['INFO12']
+    mainDocMapDetails['DOC_LIST'] = allDocCmpData['MAIN']['INFO13']
+    mainDocMapDetails['DEF_IMG'] = allDocCmpData['MAIN']['INFO14']
+    mainDocMapDetails['LIST_REF_INFO'] = allDocCmpData['MAIN']['INFO15']
+    mainDocMapDetails['IMAGE_INFO'] = allDocCmpData['MAIN']['INFO16']
+    mainDocMapDetails['IMAGE_PRO_INFO'] = allDocCmpData['MAIN']['INFO17']
+    mainDocMapDetails['MULTI_INFO'] = allDocCmpData['MAIN']['INFO18']
+    mainDocMapDetails['FORM_INFO'] = allDocCmpData['MAIN']['INFO19']
+    mainDocMapDetails['COMMON_DATA'] = allDocCmpData['MAIN']['INFO20']
+
+  } else {
+    displayOutput('MAIN Doc details is not found !!')
+  }
+  
+
+}
+
+// MAP Documents Details
+function mapDocumentDetails(docID) {  
+
+  if(docID in allDocCmpData) {
+
+    // Text Information
+    docMapDetails['TEXT_1'] = allDocCmpData[docID]['INFO0']
+    docMapDetails['TEXT_2'] = allDocCmpData[docID]['INFO1']
+    docMapDetails['TEXT_3'] = allDocCmpData[docID]['INFO2']
+    docMapDetails['TEXT_4'] = allDocCmpData[docID]['INFO3']
+    docMapDetails['TEXT_5'] = allDocCmpData[docID]['INFO4']
+
+    // Multi Text Information
+    docMapDetails['MULTI_1'] = allDocCmpData[docID]['INFO5']
+    docMapDetails['MULTI_2'] = allDocCmpData[docID]['INFO6']
+
+    // Control Information
+    docMapDetails['CNT_1'] = allDocCmpData[docID]['INFO22']
+
+    // LIST REF Information
+    docMapDetails['LIST_1'] = docID + '#INFO23'
+
+    // MAP Development and Production Image correctly .....
+    if(is_production_mode) {
+
+      // IMAGES Production Information
+      docMapDetails['IMG_1'] = docID + '#INFO10'
+      docMapDetails['IMG_2'] = docID + '#INFO12'
+      docMapDetails['IMG_3'] = docID + '#INFO14'
+      docMapDetails['IMG_4'] = docID + '#INFO16'
+      docMapDetails['IMG_5'] = docID + '#INFO19'
+      docMapDetails['IMG_6'] = docID + '#INFO21'
+      docMapDetails['IMG_7'] = docID + '#INFO8'
+    } else {
+
+      // IMAGES Information
+      docMapDetails['IMG_1'] = docID + '#INFO11'
+      docMapDetails['IMG_2'] = docID + '#INFO13'
+      docMapDetails['IMG_3'] = docID + '#INFO15'
+      docMapDetails['IMG_4'] = docID + '#INFO18'
+      docMapDetails['IMG_5'] = docID + '#INFO20'
+      docMapDetails['IMG_6'] = docID + '#INFO7'
+      docMapDetails['IMG_7'] = docID + '#INFO9'
+
+    }
+
+    // TREE Information
+    docMapDetails['TREE_1'] = docID + '#INFO24'
+
+    // FORM Information
+    docMapDetails['FORM_1'] = docID + '#INFO25'
+
+    
+
+    
+
+  } else {
+    displayOutput(docID + ' Data not found !!')
+  }
+
+}
+
+// *******************************************************
+// --------------- Extract Functions ---------------------
+
+// Extract Image URL Details
+function getImageUrl(details) {
+
+  var docID = details.split('#')[0]
+  var info_details = details.split('#')[1]
+
+  // Check Visible Status
+  if(allDocCmpData[docID][info_details+'_INFO4']) {
+     // Check Source Status
+     var image_url = 'NA'
+     if(allDocCmpData[docID][info_details+'_INFO5']) {
+            // Return DB Url
+            image_url = allDocCmpData[docID][info_details+'_INFO1']
+     } else {
+            // Return External Url
+            image_url = allDocCmpData[docID][info_details+'_INFO3']
+     }
+
+     if(image_url == 'NA') {
+       return mainDocMapDetails['DEF_IMG']
+     } else {
+       return image_url
+     }
+
+  } else {
+       // Return Collection Default Image Url
+       return mainDocMapDetails['DEF_IMG']
+  }
+
+}
+
+// Get Image Description details
+function getImageDesc(details) {
+  var docID = details.split('#')[0]
+  var info_details = details.split('#')[1]
+
+  return allDocCmpData[docID][info_details+'_INFO6']
+}
 
 
 // ******************************************************
@@ -230,6 +386,40 @@ function displayOutput(details) {
 }
 
 
+// *******************************************************
+// --------- Presentation Layer --------------------------
+// - This Layer change according to projects
+// - Sequence of all HTML modification code
+// *******************************************************
+
+// Update Header Image
+function updateHeaderImages() {
+
+  // Update HTML Page
+  var header_carousel_1_line = '<div class="item active">\
+  <img src="' + getImageUrl(docMapDetails['IMG_1']) +'" alt="New York" width="1200" height="700">\
+  <div class="carousel-caption">\
+  <h3>New York</h3>\
+  <p>' + getImageDesc(docMapDetails['IMG_1']) +'</p>\
+  </div> </div>\
+  <div class="item">\
+  <img src="' + getImageUrl(docMapDetails['IMG_2']) +'" alt="Chicago" width="1200" height="700">\
+  <div class="carousel-caption">\
+  <h3>Chicago</h3>\
+  <p>' + getImageDesc(docMapDetails['IMG_2']) +'</p>\
+  </div> </div>\
+  <div class="item">\
+  <img src="' + getImageUrl(docMapDetails['IMG_3']) +'" alt="Los Angeles" width="1200" height="700">\
+  <div class="carousel-caption">\
+  <h3>LA</h3>\
+  <p>' + getImageDesc(docMapDetails['IMG_3']) +'</p>\
+  </div></div>';
+
+  $("#header_carousel_1").html(header_carousel_1_line);
+
+}
+
+// *********************************************************
 // --------------------------- EXTRA MODEL ------------------------------
 /**
  * Displays overlay with "Please wait" text. Based on bootstrap modal. Contains animated progress bar.
